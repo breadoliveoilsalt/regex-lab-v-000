36a56734e7703f0685be1017f524def2ad09124a
Tony Distinti <adistinti@gmail.com>
2017-09-21 22:54:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w\w\w\w\w\b/)
+  text.scan(/\b\w{5}b/)
+#  text.scan(/\b\w\w\w\w\w\b/)
 #  text.scan(/(.....)/)
 end
 
@@ -23,5 +24,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.map
 end

46cffaea79184174d2bf5a3f7d93645ca8965b90
Tony Distinti <adistinti@gmail.com>
2017-09-21 22:15:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,6 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  binding.pry
   text.scan(/un\w+ing)
   #Works in text websites but not here:   text.scan(/\bun.*ing/).  it does not return an array, just the original text.
   #Yet this produces an array: text.scan(/un/).  So does this: text.scan(/un\w+/).

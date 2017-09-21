f406a7ffd108cc8425577ecc42ae1ec8ece9c863
Tony Distinti <adistinti@gmail.com>
2017-09-21 22:15:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing)
+  text.scan(/un\w+ing/)
   #Works in text websites but not here:   text.scan(/\bun.*ing/).  it does not return an array, just the original text.
   #Yet this produces an array: text.scan(/un/).  So does this: text.scan(/un\w+/).
   #The answer is, for some reason, .* picks up the whole string.  probably b/c the last work ends in ing...so it technically matches the requirements of the regex.
